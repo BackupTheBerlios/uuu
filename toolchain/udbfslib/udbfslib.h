549113f3a4011975b6ad8b97904c6cdf0603c4e4
instinc <instinc>
2003-09-20 14:54:38 +0000

    added UDBFS_INODE structure

--- toolchain/udbfslib/udbfslib.h
+++ toolchain/udbfslib/udbfslib.h
@@ -5,11 +5,27 @@
 typedef uint64_t		udate;
 
 #define UDBFS_MAGIC		0x75646221
+#define DATATYPE_INT1           0x01
+#define DATATYPE_INT2           0x02
+#define DATATYPE_INT4           0x03
+#define DATATYPE_INT8           0x04
+#define DATATYPE_INT16          0x05
+#define DATATYPE_INT32          0x06
+#define DATATYPE_INT64          0x07
+#define DATATYPE_INT128         0x08
+#define DATATYPE_CHAR           0xF0
+#define DATATYPE_VARCHAR        0xF1
+#define DATATYPE_FLOAT          0xE0
+#define DATATYPE_DATA           0xD0
+#define DATATYPE_SHAREDDATA     0xC1
+#define DATATYPE_DATETIME       0xB0
+#define DATATYPE_ENUMERATION    0xA0
 
 
 struct udbfs_mount_struct;
 struct udbfs_inode_struct;
 struct udbfs_table_struct;
+
 
 
 
@@ -40,8 +56,8 @@ struct udbfs_superblock {
 
 
 
-struct udbfs_mount {
-  struct udbfs_mount		*next,
+struct udbfs_mount_struct {
+  struct udbfs_mount_struct	*next,
 				*previous;
 
   int				block_device;
@@ -54,7 +70,20 @@ struct udbfs_mount {
 
 
 
-typedef struct udbfs_mount UDBFS_MOUNT;
+
+struct udbfs_inode_struct {
+  struct udbfs_inode_struct	*next,
+  				*previous;
+  uint32_t			id;
+  uint64_t			file_size,
+  				current_offset;
+  struct udbfs_block_struct	*blocks,
+  				*current_block;
+};
+
+
+typedef struct udbfs_inode_struct UDBFS_INODE;
+typedef struct udbfs_mount_struct UDBFS_MOUNT;
 typedef struct udbfs_superblock UDBFS_SUPERBLOCK;
 
 
