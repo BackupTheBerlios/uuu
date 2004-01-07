c34bdfa73c306d6d7e073a1eedce4f30cc3316d5
bitglue <bitglue>
2004-01-07 06:26:41 +0000

    mockfs rev 2; several bugfixes and such

--- uuu/lib/c/i386/kludges.c
+++ uuu/lib/c/i386/kludges.c
@@ -1,20 +1,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdio.h>
 
 
 
-off_t file_offset = 0;
-char file[] = "moo\n";
-unsigned file_size = 4;
 unsigned screen_pos = 0;
 uint16_t *screen = (uint16_t *)0xb8000;
+
+int memfs_addfile (const char *filename, void *data, unsigned long filesize);
+int memfs_init();
 
 #define memfs_limit_fds		64
 #define memfs_limit_files	32
 
 unsigned screen_width = 80;
 unsigned screen_height = 25;
+unsigned long numfiles = 0;
 
 
 struct _memfs_fd
@@ -30,10 +32,10 @@ struct _memfs_filedata
 {
   const char *data;
   unsigned long filesize;
-  const char filename[];
+  char filename[64];
 } memfs_files[memfs_limit_files];
 
-unsigned long numfiles = 0;
+
 
 int write(int fd, const void *buf, size_t count)
 {
@@ -78,7 +80,8 @@ int write(int fd, const void *buf, size_t count)
 
 int read(int fd, void *buf, size_t count)
 {
-  if (memfs_fd[fd].bInUse == 1 && buf != 0 && count > 0)
+  if( count == 0 ) return 0;
+  if (memfs_fd[fd].bInUse == 1 )
   {
     unsigned long truecount = count;
 
@@ -86,12 +89,9 @@ int read(int fd, void *buf, size_t count)
     if (count + memfs_fd[fd].file_pos_cur > memfs_fd[fd].file_pos_end)
       truecount = memfs_fd[fd].file_pos_end - memfs_fd[fd].file_pos_cur;
 
-    // Slow, I know, but it works... copy 1 byte at a time
-    int i;
-    char *buffer = (char *)buf;
-    for (i=0; i<truecount; i++)
-      buffer[i] = memfs_fd[fd].data[memfs_fd[fd].file_pos_cur++];
-    return i;
+    memcpy( buf, &memfs_fd[fd].data[ memfs_fd[fd].file_pos_cur ], truecount );
+    memfs_fd[fd].file_pos_cur += truecount;
+    return truecount;
   }
   errno = EINVAL;
   return -1;
@@ -110,17 +110,17 @@ off_t lseek(int fd, off_t offset, int whence)
   switch( whence )
   {
     case SEEK_SET:
-      printf("\nlseek(%u,%u,SEEK_SET)\n",fd,offset);
+      printf("\nlseek(%u,%lu,SEEK_SET)\n",fd,offset);
       memfs_fd[fd].file_pos_cur = offset;
       break;
 
     case SEEK_CUR:
-      printf("\nlseek(%u,%u,SEEK_CUR)\n",fd,offset);
+      printf("\nlseek(%u,%lu,SEEK_CUR)\n",fd,offset);
       memfs_fd[fd].file_pos_cur += offset;
       break;
 
     case SEEK_END:
-      printf("\nlseek(%u,%u,SEEK_END)\n",fd,offset);
+      printf("\nlseek(%u,%lu,SEEK_END)\n",fd,offset);
       memfs_fd[fd].file_pos_cur = memfs_fd[fd].file_pos_end + offset;
       break;
 
@@ -149,6 +149,8 @@ off_t lseek(int fd, off_t offset, int whence)
   return memfs_fd[fd].file_pos_cur;
 }
 
+
+
 int memfs_addfile (const char *filename, void *data, unsigned long filesize)
 {
   if (numfiles < memfs_limit_files)
@@ -162,17 +164,24 @@ int memfs_addfile (const char *filename, void *data, unsigned long filesize)
   return 0;
 }
 
+
+
 int memfs_init()
 {
   int i=0;
-  for (i=0; i<memfs_limit_fds; i++)
+  for (i=0; i<3; i++)
+    memfs_fd[i].bInUse = 1;
+  for (i=3; i<memfs_limit_fds; i++)
     memfs_fd[i].bInUse = 0;
 
   strcpy(memfs_files[0].filename, "stdin");
   strcpy(memfs_files[1].filename, "stdout");
   strcpy(memfs_files[2].filename, "stderr");
   numfiles = 3;
+  return 0;
 }
+
+
 
 int close(int fd)
 {
@@ -187,65 +196,58 @@ int close(int fd)
   }
   errno = EBADF;
   return -1;
-
-  if( fd == 0 || fd > 3 ) {
-    errno = EBADF;
-    return -1;
-  }
-  return 0;
 }
 
 
 
 int open(const char *pathname, int flags, ...)
 {
-  char bUse = 0;
+  int bUse = 0;
+  int bVal = 0;
   char bAvail = 0;
   char bFound = 0;
-  char bVal = 0;
-  if (pathname != 0)
-  {
-    int i=0;
-    for (i=0; i<numfiles; i++)
-    {
-      if (strcmp(pathname, memfs_files[i].filename) == 0)
-      {
-        bFound = 1;
-        bVal = i;
-        i = numfiles;
-      }
-    }
-
-    if (bFound == 1)
-    {
-      for (i=0; i<memfs_limit_fds; i++)
-      {
-        if (memfs_fd[i].bInUse == 1)
-          continue;
-        bUse = i;
-        bAvail = 1;
-        i = memfs_limit_fds;
-      }
-    }
-
-    if (bAvail == 1)
-    {
-      memfs_fd[bUse].bInUse = 1;
-      memfs_fd[bUse].data = memfs_files[bVal].data;
-      memfs_fd[bUse].file_pos_start = 0;
-      memfs_fd[bUse].file_pos_end = memfs_files[bVal].filesize;
-      memfs_fd[bUse].file_pos_cur = 0;
-      return bUse;
-    }
-  }
 
   if( (flags & O_ACCMODE) != O_RDONLY ) {
     errno = EROFS;
     return -1;
   }
-  file_offset = 0;
-  return 3;
+
+  unsigned i;
+  for (i=0; i<numfiles; i++)
+  {
+    if (strcmp(pathname, memfs_files[i].filename) == 0)
+    {
+      bFound = 1;
+      bVal = i;
+      i = numfiles;
+    }
+  }
+
+  if (bFound == 1)
+  {
+    for (i=0; i<memfs_limit_fds; i++)
+    {
+      if (memfs_fd[i].bInUse == 1)
+	continue;
+      bUse = i;
+      bAvail = 1;
+      i = memfs_limit_fds;
+    }
+  }
+
+  if (bAvail == 1)
+  {
+    memfs_fd[bUse].bInUse = 1;
+    memfs_fd[bUse].data = memfs_files[bVal].data;
+    memfs_fd[bUse].file_pos_start = 0;
+    memfs_fd[bUse].file_pos_end = memfs_files[bVal].filesize;
+    memfs_fd[bUse].file_pos_cur = 0;
+    return bUse;
+  }
+  errno = ENOENT;
+  return -1;
 }
+
 
 
 int rename(const char *oldpath, const char *newpath)
